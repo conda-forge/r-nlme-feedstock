9ddbea83e7ce62fc8e331114105574d60c13fb96
Isuru Fernando <isuruf@gmail.com>
2021-01-26 23:29:38 -0600

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,5 @@
 
 export DISABLE_AUTOBREW=1
 
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
-  rsync -a -I $BUILD_PREFIX/lib/R/library/ $PREFIX/lib/R/library/
-fi
-
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
